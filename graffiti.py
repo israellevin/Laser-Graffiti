186bed3c413a64b7635fabf9731df3075624f666
root <israellevin@fastmail.fm>
2010-04-01 23:18:18 +0300

    Geometry and coefs, all to naught

--- graffiti.py
+++ graffiti.py
@@ -2,13 +2,15 @@ import VideoCapture
 import Image
 import pyglet
 import math
-import itertools
+
+import time
 
 
 class Coord:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.xy = (x, y)
     def flat(self):
         yield self.x
         yield self.y
@@ -38,15 +40,10 @@ class Triangle:
         self.ab = Line(a, b)
         self.bc = Line(b, c)
         self.ca = Line(c, a)
-        self.alpha = math.degrees(math.acos((self.bc.sqrlen + self.ca.sqrlen - self.ab.sqrlen) / (2.0 * self.bc.length * self.ca.length)))
-        self.beta = math.degrees(math.acos((self.ab.sqrlen + self.ca.sqrlen - self.bc.sqrlen) / (2.0 * self.ab.length * self.ca.length)))
-        self.gamma = math.degrees(math.acos((self.ab.sqrlen + self.bc.sqrlen - self.ca.sqrlen) / (2.0 * self.ab.length * self.bc.length)))
-
-a = Coord(0, 0)
-b = Coord(0, 3)
-c = Coord(4, 0)
-t = Triangle(a, b, c)
-print t.alpha, t.beta, t.gamma
+        #self.alpha = math.degrees(math.acos((self.bc.sqrlen + self.ca.sqrlen - self.ab.sqrlen) / (2.0 * self.bc.length * self.ca.length)))
+        #self.beta = math.degrees(math.acos((self.ab.sqrlen + self.ca.sqrlen - self.bc.sqrlen) / (2.0 * self.ab.length * self.ca.length)))
+        #self.gamma = math.degrees(math.acos((self.ab.sqrlen + self.bc.sqrlen - self.ca.sqrlen) / (2.0 * self.ab.length * self.bc.length)))
+        self.hca = math.sqrt((self.ab.length + self.bc.length + self.ca.length) * (self.ab.length + self.bc.length - self.ca.length) * (self.ab.length - self.bc.length + self.ca.length) * (- self.ab.length + self.bc.length + self.ca.length)) / (2 * self.ca.length)
 
 
 class Cam(VideoCapture.Device):
@@ -87,30 +84,43 @@ class Quad:
 
     def calculate(self):
         self.matrix = []
-
-        nw = self.coords[0:2]
-        sw = self.coords[2:4]
-        se = self.coords[4:6]
-        ne = self.coords[6:8]
+        sw = self.coords[0].xy
+        nw = self.coords[1].xy
+        ne = self.coords[2].xy
+        se = self.coords[3].xy
         x0, y0 = nw
-        As = 1.0 / projection.width
-        At = 1.0 / projection.height
-        coefs = (x0, (ne[0]-x0)*As, (sw[0]-x0)*At,
-                (se[0]-sw[0]-ne[0]+x0)*As*At,
-                y0, (ne[1]-y0)*As, (sw[1]-y0)*At,
-                (se[1]-sw[1]-ne[1]+y0)*As*At)
-
-        d = min(self.coords[1], self.coords[3], self.coords[5], self.coords[7])
-        l = min(self.coords[0], self.coords[2], self.coords[4], self.coords[6])
-        u = max(self.coords[1], self.coords[3], self.coords[5], self.coords[7])
-        r = max(self.coords[0], self.coords[2], self.coords[4], self.coords[6])
-
+        As = 1.0 / projection.width #camSize[0]
+        At = 1.0 / projection.height #camSize[1]
+        a0 = x0
+        a1 = (ne[0]-x0)*As
+        a2 = (sw[0]-x0)*At
+        a3 = (se[0]-sw[0]-ne[0]+x0)*As*At
+        a4 = y0
+        a5 = (ne[1]-y0)*As
+        a6 = (sw[1]-y0)*At
+        a7 = (se[1]-sw[1]-ne[1]+y0)*As*At
+        l = min([c.x for c in self.coords])
+        r = max([c.x for c in self.coords])
+        d = min([c.y for c in self.coords])
+        u = max([c.y for c in self.coords])
         for row in range(d, u + 1):
             for col in range(l, r + 1):
-                x = int(round((coefs[0] * col + coefs[1] * row + coefs[2]) / (coefs[6] * col + coefs[7] * row + 1)))
-                y = int(round((coefs[3] * col + coefs[4] * row + coefs[5]) / (coefs[6] * col + coefs[7] * row + 1)))
-                if x > 0 and x < projection.width and y > 0 and y < projection.height:
-                    self.matrix.append((row, col, x, y))
+                x = a0 + a1*col + a2*row + a3*col*row;
+                y = a4 + a5*col + a6*row + a7*col*row;
+                if x < 0 or y < 0 or x > camSize[0] or y > camSize[1]:
+                    print x, y
+                else:
+                    self.matrix.append((int(x), int(y)))
+#b = Coord(row, col)
+#altitudes = []
+#for i in range(4):
+#    a = self.coords[i]
+#    c = self.coords[(i + 1) % 4]
+#    t = Triangle(a, b, c)
+#    altitudes.append(t.hca)
+#x = row * (altitudes[1] / altitudes[3])
+#y = row * (altitudes[0] / altitudes[2])
+#self.matrix.append((row, col, x, y))
 
 quad = Quad()
 
@@ -143,15 +153,17 @@ def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.ESCAPE:
         pyglet.app.exit()
     if symbol == pyglet.window.key.RETURN:
+        t = time.time()
         quad.calculate()
-        print quad.matrix[:10]
+        print len(quad.matrix)
+        print len(set(quad.matrix))
         camimg = cam.getImage()
         campix = camimg.tostring()
         projimg = projection.img
         projpix = [0 for i in range(projimg.width * projimg.height * 3)]
         for point in quad.matrix:
-            cami = ((point[0] + point[1] * camimg.size[0]) * 3)
-            proji = ((point[1] + point[2] * projimg.width) * 3)
+            cami = int(((point[0] + point[1]) * camimg.size[0]) * 3)
+            proji = int(((point[1] + point[2]) * projimg.width) * 3)
             try:
                 for i in range(3):
                     projpix[proji + i] = 255 #ord(campix[cami + i])
